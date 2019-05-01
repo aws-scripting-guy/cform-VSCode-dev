ee595321fa3a12dec1a196cbe8bbfaef98b1ac7a
AWS Scripting Guy <aws-scripting-guy@outlook.com>
2019-05-01 15:24:52 +0200

    mapped system variable

--- scripts/git-package.ps1
+++ scripts/git-package.ps1
@@ -1,6 +1,6 @@
 # Write package.json back to github only for release builds 
 
-if ($(Build.Reason) -eq "Schedule") {
+if ($BUILD_REASON -eq "Schedule") {
   # write package.json back to repo 
   git show
   git log --oneline
